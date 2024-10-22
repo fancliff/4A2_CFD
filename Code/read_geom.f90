28e9eb34fb299062d94c79d2ad1936b318fee963
F.R.J. Ancliff <frja2@tw403.eng.cam.ac.uk>
2024-10-22 15:56:13 +0100

    read_geom.f90 completed

--- Code/read_geom.f90
+++ Code/read_geom.f90
@@ -10,7 +10,7 @@
       type(t_geometry), intent(out) :: geom
       
 !     Declare an integer to count the number of points in the curve as you loop
-!     INSERT
+      integer :: n
 
 !     Open the file and assign to unit 1
       open(1,file='geom_' // av%casename // '.txt')
@@ -22,19 +22,28 @@
 
 !     Read the x and y coordinates of the curve from the file, create a do loop
 !     that iterates from 1 to "geom%ni_a", the length of the first domain curve
-!     INSERT 
+      do n = 1, geom%ni_a
+          read(1,*) geom%x_a(n), geom%y_a(n)
+      end do
 
 !     Repeat the process for the second curve, read its length, allocate the
 !     memory, then read the coordinates line by line from the file
-!     INSERT
+      read(1,*) geom%ni_b
+      allocate(geom%x_b(geom%ni_b),geom%y_b(geom%ni_b))
+      
+      do n = 1, geom%ni_b
+          read(1,*) geom%x_b(n), geom%y_b(n)
+      end do
 
 !     Print the lengths of the curves that have been successfully read
       write(6,*) 'Read domain curves from file'
       write(6,*) '  Curve lengths ni_a =', geom%ni_a, 'ni_b = ', geom%ni_b
+!      write(6,*) 'Curve b x values:', geom%x_b
+!      write(6,*) 'Curve b y values:', geom%y_b
       write(6,*)
 
 !     Close the unit now everything has been read
-!     INSERT
+      close(1)
 
       end subroutine read_geom
 
