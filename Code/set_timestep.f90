8aac2644afd9a48f32e87f42d27d2136ae7a6a81
F.R.J. Ancliff <frja2@tw213.eng.cam.ac.uk>
2024-10-23 15:39:36 +0100

    basic timestep implemented

--- Code/set_timestep.f90
+++ Code/set_timestep.f90
@@ -14,19 +14,25 @@
 
 !     Calculate the stagnation speed of sound from the inlet stagnation
 !     temperature and gas constants
-!     INSERT
+      astag = sqrt(av%gam * av%rgas * bcs%tstag)
 
 !     Assume that the maximum flow speed is also equal to "astag". This will 
 !     be pessimistic for subsonic flows but may be optimistic for supersonic 
 !     flows. In the latter case the length of the time step as determined by 
 !     may need to be reduced by improving this routine or varying the CFL number
-!     INSERT
+      v_max = astag
 
 !     Calculate the timestep using the CFL number and store it in "av%dt"
-!     INSERT
+      av%dt = av%cfl * g%l_min / (2 * v_max)
 
 !     Print the calculated timestep and some intermediate values
-!     INSERT
+      write(6,*) 'a(stag): ', astag
+      write(6,*) 'V max: ', v_max
+      write(6,*) 'CFL no. ', av%cfl
+      write(6,*) 'L min: ', g%l_min
+      write(6,*)
+      write(6,*) 'Timestep: ', av%dt
+      write(6,*)
 
       end subroutine set_timestep
 
