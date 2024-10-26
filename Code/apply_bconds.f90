438bf92dbc15db6ae60c51ba24ffe80b467043ee
F.R.J. Ancliff <frja2@millenicut.eng.cam.ac.uk>
2024-10-26 15:21:36 +0100

    finished apply_bconds

--- Code/apply_bconds.f90
+++ Code/apply_bconds.f90
@@ -13,7 +13,7 @@
       type(t_bconds), intent(inout) :: bcs
 
 !     Declare the other variables you need here
-!     INSERT
+      real, dimension(g%nj) :: tstat, vel
 
 !     At the inlet boundary the change in density is driven towards "rostag",
 !     which is then used to obtain the other flow properties to match the
@@ -36,11 +36,19 @@
 !     Calculate "p(1,:)", "rovx(1,:)", "rovy(1,:)" and "roe(1,:)" from the inlet 
 !     "ro(:)", "pstag", "tstag" and "alpha". Also set "vx(1,:)", "vy(1,:)" and 
 !     "hstag(1,:)"
-!     INSERT
+      tstat = bcs%tstag * (bcs%ro/bcs%rostag)**(av%gam-1)
+      vel = sqrt(2.0 * av%cp * (bcs%tstag-tstat))
+
+      g%rovx(1,:) = bcs%ro(:) * vel(:) * cos(bcs%alpha)
+      g%rovy(1,:) = bcs%ro(:) * vel(:) * sin(bcs%alpha)
+    
+      g%roe(1,:) = bcs%ro * (av%cv*tstat + vel**2 / 2)
+      
+      g%p(1,:) = bcs%pstag * (bcs%ro / bcs%rostag)**av%gam
 
 !     For the outlet boundary condition set the value of "p(ni,:)" to the
 !     specified value of static pressure "p_out" in "bcs"
-!     INSERT
+      g%p(g%ni,:) = bcs%p_out
 
       end subroutine apply_bconds
 
