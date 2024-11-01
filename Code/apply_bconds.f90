fb61ca62fc4e5cb366a881407343d60ef0e3805a
F.R.J. Ancliff <frja2@millenicut.eng.cam.ac.uk>
2024-11-01 12:10:18 +0000

    debugging apply bconds as bend case is not working - going supersonic at inlet ???

--- Code/apply_bconds.f90
+++ Code/apply_bconds.f90
@@ -37,7 +37,7 @@
 !     "ro(:)", "pstag", "tstag" and "alpha". Also set "vx(1,:)", "vy(1,:)" and 
 !     "hstag(1,:)"
       tstat = bcs%tstag * (bcs%ro/bcs%rostag)**(av%gam-1)
-      vel = sqrt(2.0 * av%cp * (bcs%tstag-tstat))
+      vel = sqrt( (2.0 * av%gam * av%rgas) * (bcs%tstag-tstat)  / (av%gam - 1) )
 
       g%rovx(1,:) = bcs%ro(:) * vel(:) * cos(bcs%alpha)
       g%rovy(1,:) = bcs%ro(:) * vel(:) * sin(bcs%alpha)
@@ -45,6 +45,10 @@
       g%roe(1,:) = bcs%ro * (av%cv*tstat + vel**2 / 2)
       
       g%p(1,:) = bcs%pstag * (bcs%ro / bcs%rostag)**av%gam
+      
+      g%vx(1,:) = vel(:) * cos(bcs%alpha)
+      g%vy(1,:) = vel(:) * sin(bcs%alpha)
+      g%hstag(1,:) = (g%roe(1,:) + g%p(1,:)) / g%ro(1,:)
 
 !     For the outlet boundary condition set the value of "p(ni,:)" to the
 !     specified value of static pressure "p_out" in "bcs"
