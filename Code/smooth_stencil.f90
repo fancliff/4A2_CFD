db4563c74777f1dc91f99c58306ceefd8b96152d
F.R.J. Ancliff <frja2@tw408.eng.cam.ac.uk>
2024-11-19 14:12:25 +0000

    potentially finished 4th order smoothing

--- Code/smooth_stencil.f90
+++ Code/smooth_stencil.f90
@@ -66,16 +66,19 @@
 !     Work out i direction first then add j direction
       
       if 4th_smooth then
+
 !     i direction - central:
           prop_avg_4_i(3:ni-2,1:nj) = - 1/6 * prop(1:ni-4,1:nj) 
                                       + 2/3 * prop(2:ni-3,1:nj)
                                       + 2/3 * prop(4:ni-1,1:nj)
                                       - 1/6 * prop(5:ni  ,1:nj)
+
 !     j direction - central:
           prop_avg_4_j(1:ni,3:nj-2) = - 1/6 * prop(1:ni,1:nj-4)
                                       + 2/3 * prop(1:ni,2:nj-3)
                                       + 2/3 * prop(1:ni,4:nj-1)
                                       - 1/6 * prop(1:ni,5:nj  )
+
 !      i direction - semi one-sided:
           prop_avg_4_i(2,1:nj) = + 1/4 * prop(1,1:nj)
                                  + 3/2 * prop(3,1:nj)
@@ -86,7 +89,42 @@
                                     + 3/2 * prop(ni-2,1:nj)
                                     - 1   * prop(ni-3,1:nj)
                                     + 1/4 * prop(ni-4,1:nj)                                 
-          
+
+!      j direction - semi one-sided:
+          prop_avg_4_j(1:ni,2) = + 1/4 * prop(1:ni,1)
+                                 + 3/2 * prop(1:ni,3)
+                                 - 1   * prop(1:ni,4)
+                                 + 1/4 * prop(1:ni,5)
+
+          prop_avg_4_j(1:ni,nj-1) = + 1/4 * prop(1:ni,nj)
+                                    + 3/2 * prop(1:ni,nj-2)
+                                    - 1   * prop(1:ni,nj-3)
+                                    + 1/4 * prop(1:ni,nj-4)
+
+!     i direction - full one-sided (not corners):                                           
+          prop_avg_4_i(1,2:nj-1) = + 4 * prop(2,2:nj-1)
+                                   - 6 * prop(3,2:nj-1)
+                                   + 4 * prop(4,2:nj-1)
+                                   - 1 * prop(5,2:nj-1)
+                                   
+          prop_avg_4_i(ni,2:nj-1) = + 4 * prop(ni-1,2:nj-1)
+                                    - 6 * prop(ni-2,2:nj-1)
+                                    + 4 * prop(ni-3,2:nj-1)
+                                    - 1 * prop(ni-4,2:nj-1)
+                                    
+!     j direction - full one-sided (not corners):                                           
+          prop_avg_4_j(2:ni-1,1) = + 4 * prop(2:ni-1,2)
+                                   - 6 * prop(2:ni-1,3)
+                                   + 4 * prop(2:ni-1,4)
+                                   - 1 * prop(2:ni-1,5)
+                                   
+          prop_avg_4_j(2:ni-1,nj) = + 4 * prop(2:ni-1,2)
+                                    - 6 * prop(2:ni-1,3)
+                                    + 4 * prop(2:ni-1,4)
+                                    - 1 * prop(2:ni-1,5)
+
+!     Add the i and j direction contributions together
+      prop_avg_4 = prop_avg_4_i + prop_avg_4_j
 
 !     Now apply the artificial viscosity by smoothing "prop" towards "prop_avg",
 !     take (1-sfac) * the calculated value of the property + sfac * the average 
