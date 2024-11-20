5c3744651a212bdbf4c8c892a2d1ffa8598841ce
F.R.J. Ancliff <frja2@tw408.eng.cam.ac.uk>
2024-11-20 16:03:15 +0000

    attempting to debug 4th order smoothing in separate area without any other changes over interim (except minor change to read settings)

---
+++ Code/fourth_smoothing/euler_iteration.f90
@@ -0,0 +1,77 @@
+
+      subroutine euler_iteration(av,g)
+
+!     This subroutine calculates the fluxes into each cell and then sums them to
+!     update the primary flow properties
+
+!     Explicitly declare the required variables
+      use types
+      use flux_stencil
+      use smooth_stencil
+      implicit none
+      type(t_appvars), intent(in) :: av
+      type(t_grid), intent(inout) :: g
+      real, dimension(g%ni,g%nj-1) :: mass_i, flux_i
+      real, dimension(g%ni-1,g%nj) :: mass_j, flux_j
+      integer :: i, j, ni, nj
+
+!     Get the block size and store locally for convenience
+      ni = g%ni; nj = g%nj
+
+!     Setup the continuity equation by calculating the mass flow through
+!     the facets in both the i and j-directions. Store these values in
+!     "mass_i" and "mass_j"
+      mass_i = ((g%rovx(:,1:nj-1)+g%rovx(:,2:nj))*g%lx_i + &
+                (g%rovy(:,1:nj-1)+g%rovy(:,2:nj))*g%ly_i) / 2.0
+      
+      mass_j = ((g%rovx(1:ni-1,:)+g%rovx(2:ni,:))*g%lx_j + &
+                (g%rovy(1:ni-1,:)+g%rovy(2:ni,:))*g%ly_j) / 2.0
+     
+!     Apply the wall boundary condition by checking that two nodes at the
+!     end of a facet are both on a wall, if so then the appropriate mass
+!     flow array is set to have zero flow through that facet
+      where(g%wall(1:ni-1,:) .and. g%wall(2:ni,:)) mass_j = 0.0
+      where(g%wall(:,1:nj-1) .and. g%wall(:,2:nj)) mass_i = 0.0
+
+!     Update the density with mass fluxes by calling "sum_fluxes"
+      call sum_fluxes(av,mass_i,mass_j,g%area,g%ro,g%dro)
+
+!     Setup the conservation of energy equation by calculated the enthalpy flux
+!     and storing the values in "flux_i" and "flux_j", you will need "mass_i"
+!     and "mass_j" from before
+      flux_i = (g%hstag(:,1:nj-1) + g%hstag(:,2:nj)) * mass_i / 2.0
+      flux_j = (g%hstag(1:ni-1,:) + g%hstag(2:ni,:)) * mass_j / 2.0
+
+!     Update the internal energy with enthalpy fluxes
+      call sum_fluxes(av,flux_i,flux_j,g%area,g%roe,g%droe)
+
+!     Setup the x-momentum equation including momentum flux and pressure forces
+      flux_i = ( mass_i*(g%vx(:,1:nj-1) + g%vx(:,2:nj)) + &
+                (g%p(:,1:nj-1) + g%p(:,2:nj))*g%lx_i ) / 2.0
+                
+      flux_j = ( mass_j*(g%vx(1:ni-1,:) + g%vx(2:ni,:)) + &
+                (g%p(1:ni-1,:) + g%p(2:ni,:))*g%lx_j ) / 2.0
+
+!     Update the x-momentum with momentum flux
+      call sum_fluxes(av,flux_i,flux_j,g%area,g%rovx,g%drovx)
+
+!     Setup the y-momentum equation including momentum flux and pressure forces
+      flux_i = ( mass_i*(g%vy(:,1:nj-1) + g%vy(:,2:nj)) + &
+                (g%p(:,1:nj-1) + g%p(:,2:nj))*g%ly_i ) / 2.0
+                
+      flux_j = ( mass_j*(g%vy(1:ni-1,:) + g%vy(2:ni,:)) + &
+                (g%p(1:ni-1,:) + g%p(2:ni,:))*g%ly_j ) / 2.0
+
+!     Update the y-momentum with momentum flux
+      call sum_fluxes(av,flux_i,flux_j,g%area,g%rovy,g%drovy)
+
+!     Add artificial viscosity by smoothing all of the primary flow variables
+      call smooth_array(av, g%ro, av%ro_ref)
+      call smooth_array(av, g%roe, av%roe_ref)
+      call smooth_array(av, g%rovx, av%rov_ref)
+      call smooth_array(av, g%rovy, av%rov_ref)
+      
+
+      end subroutine euler_iteration
+
+
---
+++ Code/fourth_smoothing/smooth_stencil.f90
@@ -0,0 +1,72 @@
+
+      module smooth_stencil
+
+!     Packaging a subroutine in a module allows it to recieve the data
+!     conveniently as assumed shape arrays
+      
+      contains
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      subroutine smooth_array(av,prop,prop_ref)
+
+!     This subroutine smooths "prop" to stabilise the calculation, the basic 
+!     solver uses second order smoothing, many improvements are possible.
+
+!     Explicitly declare the required variables
+      use types
+      implicit none
+      type(t_appvars), intent(in) :: av
+      real, intent(inout) :: prop(:,:)
+      real, intent(in) :: prop_ref
+      real, dimension(size(prop,1),size(prop,2)) :: prop_avg, sfac_loc
+      integer :: ni, nj
+
+!     Get the block size and store locally for convenience
+      ni = size(prop,1); nj = size(prop,2)
+
+!     Calculate the average values at the nodes in the interior region of the
+!     mesh, use the four neighbouring nodes in the plus and minus i and 
+!     j-directions.
+      prop_avg(2:ni-1,2:nj-1) = ( prop(3:ni,2:nj-1) + prop(1:ni-2,2:nj-1) &
+                                + prop(2:ni-1,3:nj) + prop(2:ni-1,1:nj-2) ) / 4.0
+
+!     Edge values are also averaged in both the i and j-directions. Parallel to
+!     the boundary the averaging is centred, the averages of two nodes are taken
+!     either side of the current point. Perpendicular to the boundary the
+!     algorithm is one-sided, the value at the current point is extrapolated
+!     from the values at two nodes away from the boundary point.
+      prop_avg(1,2:nj-1) = ( prop(1,1:nj-2) + prop(1,3:nj) &
+                           + 2.0*prop(2,2:nj-1) - prop(3,2:nj-1) ) / 3.0
+
+      prop_avg(ni,2:nj-1) = ( prop(ni,1:nj-2) + prop(ni,3:nj) &
+                           + 2.0*prop(ni-1,2:nj-1) - prop(ni-2,2:nj-1) ) / 3.0                   
+
+      prop_avg(2:ni-1,1) = ( prop(3:ni,1) + prop(1:ni-2,1) &
+                           + 2.0*prop(2:ni-1,2) - prop(2:ni-1,3) ) / 3.0
+
+      prop_avg(2:ni-1,nj) = ( prop(3:ni,nj) + prop(1:ni-2,nj) &
+                           + 2.0*prop(2:ni-1,nj-1) - prop(2:ni-1,nj-2) ) / 3.0
+
+!     The corner values are not currently smoothed
+      prop_avg([1,ni],[1,nj]) = prop([1,ni],[1,nj])
+
+!     Now apply the artificial viscosity by smoothing "prop" towards "prop_avg",
+!     take (1-sfac) * the calculated value of the property + sfac * the average 
+!     of the surrounding values. 
+      sfac_loc = av%sfac/prop_ref * abs(prop - prop_avg)
+      write(6,*)
+      write(6,*)
+      !write(6,*) sfac_loc
+      write(6,*)
+      write(6,*)
+      prop = (1.0 - sfac_loc) * prop + sfac_loc * prop_avg
+      !prop = (1 - av%sfac) * prop + av%sfac * prop_avg
+
+      end subroutine smooth_array
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+      end module smooth_stencil
+
+
